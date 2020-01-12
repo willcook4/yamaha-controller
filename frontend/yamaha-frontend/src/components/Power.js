de07cca4a71770082183a732856bf06c769b4dcf
Will Cook <hello@doubleucook.com>
2020-01-13 12:38:34 +1300

    added actions and connected power to the zonecontext state

--- frontend/yamaha-frontend/src/components/Power.js
+++ frontend/yamaha-frontend/src/components/Power.js
@@ -1,10 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Api from '../lib/Api'
-
-// const Power = (props) => {
-
-// return [(<p>TEsting {powerState} {props.zone}</p>), setPowerState]
-// }
+import { useZoneState, useZoneDispatch } from './ZoneContext'
 
 /**
  * Power
@@ -14,10 +10,31 @@ import Api from '../lib/Api'
  */
 
  const Power = (props) => {
-  const [powerState, setPowerState] = useState('NOT SURE')
-  return [(
-    <p>Power state for {props.zone}: {powerState}</p>
-  ), setPowerState]
+  const { isOn } = useZoneState()
+  const dispatch = useZoneDispatch()
+
+  const [errMsg, setErrMsg] = useState('')  
+
+  async function togglePower(zone, action) {
+    try {
+      let apiResponse = await Api.togglePowerForZone(zone, action)
+      dispatch({type: action, isOn: apiResponse.data.isOn})
+    } catch(error) {
+      setErrMsg(error.response.data.message)
+    }
+  }
+
+  return (
+    <>
+      <p>Power state for {props.zoneName}:</p>
+      <p>{isOn ? 'ON' : 'OFF'}</p>
+      {errMsg}
+      <button onClick={() => {
+        let action = isOn ? 'power-off' : 'power-on'
+        togglePower(props.zoneName, action)
+      }}>Turn {isOn ? 'OFF' : 'ON'}</button>
+    </>
+  )
  }
 
  export { Power }
